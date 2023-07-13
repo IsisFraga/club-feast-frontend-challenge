2827d986c7d5d8f0a3d56334501d721f2b6c89e8
Isis Fraga <51866290+IsisFraga@users.noreply.github.com>
2023-07-13 20:04:50 -0300

    feat: initializing next project

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'TMDB Movies',
+  description: 'See a movie tonight!',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
