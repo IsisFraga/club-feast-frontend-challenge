a858ea4dc28c6a28d0e6588bdf1f4055255e0b6b
Isis Fraga <51866290+IsisFraga@users.noreply.github.com>
2023-07-19 21:29:59 -0300

    feat: movie page components insertion

--- app/movie/[slug]/components/movie.tsx
+++ app/movie/[slug]/components/movie.tsx
@@ -1,10 +1,20 @@
+import MovieCastSection from '@/components/MovieCastSection/MovieCastSection';
+import MovieMainInfo from '@/components/MovieMainInfo/MovieMainInfo';
+import MovieNavbar from '@/components/MovieNavbar/MovieNavbar';
+import mockedMovie from '@/mocks/movieMock';
+import creditsMock from '@/mocks/creditsMock';
 import { PageProps } from '@/services/dynamic';
 
 export async function MoviePage (props: PageProps) {
   console.log('Movie Page log no server side')
   return (
-    <>
-    vai vir
-    </>
+    <div className='
+        overflow-hidden
+      '
+    >
+     <MovieNavbar />
+     <MovieMainInfo movieDetails={mockedMovie} crewDetails={creditsMock.crew} />
+     <MovieCastSection castDetails={creditsMock.cast} />
+    </div>
   )
 }
