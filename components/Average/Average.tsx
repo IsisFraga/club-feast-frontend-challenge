89a000b331f1acf508303818ab89c4cb965cfaaa
Isis Fraga <51866290+IsisFraga@users.noreply.github.com>
2023-07-19 21:27:20 -0300

    feat: average component adjustments

--- components/Average/Average.tsx
+++ components/Average/Average.tsx
@@ -1,19 +1,20 @@
 export interface IAverage {
   movieVoteAverage: number
+  mobileHidden?: boolean
 }
 
-const Average = ({movieVoteAverage}: IAverage) => {
-
+const Average = ({movieVoteAverage, mobileHidden}: IAverage) => {
   const circumference = 20 * 2 * Math.PI;
   const filledCircunference =
     circumference - (movieVoteAverage / 10) * circumference;
-  const average = movieVoteAverage !== 0 ? movieVoteAverage * 10 : "NR"
+  const average = movieVoteAverage !== 0 ? (movieVoteAverage * 10).toFixed(0) : "NR"
   const roundColor = movieVoteAverage > 6.5 ? "text-green-600" : "text-yellow-rate"
+
 
   return (
     <div>
-      <div className="hidden md:flex items-center justify-center rounded-full absolute w-10 h-10">
-        <svg width={38} height={38} className="absolute transform -rotate-90 -top-5 left-2 bg-[#081c22] rounded-full" viewBox="0 0 60 60">
+      <div className={`${mobileHidden ? "hidden absolute" : "flex"} md:flex items-center justify-center rounded-full w-10 h-10`}>
+        <svg width={38} height={38} className={`absolute transform -rotate-90 ${mobileHidden && "-top-5 left-2"} bg-[#081c22] rounded-full`} viewBox="0 0 60 60">
           <circle
             className="text-gray-600"
             strokeWidth="3"
@@ -36,7 +37,7 @@ const Average = ({movieVoteAverage}: IAverage) => {
             cy="30"
           />
         </svg>
-        <span className="text-white text-sm	relative -top-[20px] left-2 font-bold">
+        <span className={`text-white text-sm	relative font-bold ${mobileHidden && "-top-[20px] left-2"}`}>
           {average}
           {movieVoteAverage !== 0 &&
             <span className="relative text-[6px] -top-1.5">
