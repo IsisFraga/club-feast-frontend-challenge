e0262ec812d74db44a2e68cdf6da144d39bcfab0
Isis Fraga <51866290+IsisFraga@users.noreply.github.com>
2023-07-19 21:29:12 -0300

    feat: arrow icon

---
+++ public/icons/ArrowDownIcon.tsx
@@ -0,0 +1,10 @@
+const ArrowDownIcon = () => {
+  return (
+    <svg fill="CurrentColor" height="11px" width="11px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" 
+      viewBox="0 0 386.257 386.257">
+      <polygon points="0,96.879 193.129,289.379 386.257,96.879 "/>
+    </svg>
+  )
+}
+
+export default ArrowDownIcon
